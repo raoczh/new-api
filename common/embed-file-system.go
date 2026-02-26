fed81f6e45bb37e66d3db956a4e866e89cc92d56
raoczh <raocaizhu@autohome.com.cn>
2026-02-26 19:07:26 +0800

    feat: add precompressed static file serving with gzip support

--- common/embed-file-system.go
+++ common/embed-file-system.go
@@ -3,10 +3,14 @@ package common
 import (
 	"embed"
 	"io/fs"
+	"mime"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
 // Credit: https://github.com/gin-contrib/static/issues/19
@@ -39,5 +43,53 @@ func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
 	}
 	return &embedFileSystem{
 		FileSystem: http.FS(efs),
+	}
+}
+
+// ServePrecompressedStatic serves static files from embed.FS,
+// preferring pre-compressed .gz files when the client supports gzip.
+// This avoids runtime gzip compression overhead.
+func ServePrecompressedStatic(fsEmbed embed.FS, targetPath string) gin.HandlerFunc {
+	subFS, err := fs.Sub(fsEmbed, targetPath)
+	if err != nil {
+		panic(err)
+	}
+	fileServer := http.FileServer(http.FS(subFS))
+
+	return func(c *gin.Context) {
+		urlPath := strings.TrimPrefix(c.Request.URL.Path, "/")
+		if urlPath == "" {
+			c.Next()
+			return
+		}
+
+		// Check if the original file exists
+		origFile, err := subFS.Open(urlPath)
+		if err != nil {
+			c.Next()
+			return
+		}
+		origFile.Close()
+
+		// If client accepts gzip, try to serve pre-compressed .gz file
+		if strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
+			gzPath := urlPath + ".gz"
+			data, err := fs.ReadFile(subFS, gzPath)
+			if err == nil {
+				contentType := mime.TypeByExtension(filepath.Ext(urlPath))
+				if contentType == "" {
+					contentType = "application/octet-stream"
+				}
+				c.Header("Content-Encoding", "gzip")
+				c.Header("Vary", "Accept-Encoding")
+				c.Data(http.StatusOK, contentType, data)
+				c.Abort()
+				return
+			}
+		}
+
+		// Fallback: serve the original uncompressed file
+		fileServer.ServeHTTP(c.Writer, c.Request)
+		c.Abort()
 	}
 }
