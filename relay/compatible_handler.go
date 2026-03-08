c1724ed9861f5e3b194c400e7063903fd137984c
raoczh <raocaizhu@autohome.com.cn>
2026-03-08 12:14:07 +0800

    渠道配置增加转换为openai response开关

--- relay/compatible_handler.go
+++ relay/compatible_handler.go
@@ -75,7 +75,8 @@ func TextHelper(c *gin.Context, info *relaycommon.RelayInfo) (newAPIError *types
 	if info.RelayMode == relayconstant.RelayModeChatCompletions &&
 		!passThroughGlobal &&
 		!info.ChannelSetting.PassThroughBodyEnabled &&
-		service.ShouldChatCompletionsUseResponsesGlobal(info.ChannelId, info.ChannelType, info.OriginModelName) {
+		(info.ChannelSetting.UseResponsesAPI ||
+			service.ShouldChatCompletionsUseResponsesGlobal(info.ChannelId, info.ChannelType, info.OriginModelName)) {
 		applySystemPromptIfNeeded(c, info, request)
 		usage, newApiErr := chatCompletionsViaResponses(c, info, adaptor, request)
 		if newApiErr != nil {
